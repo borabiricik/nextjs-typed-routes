1aa3d69dcdc61157fa639e76c6ab9253fc414c7e
borabiricik <bora.bir496@gmail.com>
2024-04-23 20:07:58 +0300

    feat: example added for typedRoutes

---
+++ src/app/profile/details/page.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const ProfileDetailsPage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl">Profile/Details Page</h1>
+    </div>
+  )
+}
+
+export default ProfileDetailsPage
